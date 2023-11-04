79b06f9e55001370dfd7a9b4d00ab0d152e7f179
ecavalcanti683 <ecavalcanti.dg@outlook.com>
2023-11-04 12:16:49 +0100

    Done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,35 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = []; // armazenaremos os cartões em que o usuário clicou para que possamos compará-los.
+    this.pairsClicked = 0; 
+    this.pairsGuessed = 0; // onde será adicionado toda vez que um usuário escolher e adivinhar um par
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (this.cards) {
+    const shuffleArray = this.cards.sort(() => Math.random() - 0.5);
+    return shuffleArray;
+    } else {
+      return undefined;
+    }
   }
 
   checkIfPair(card1, card2) {
-    // ... write your code here
+    if (card1 === card2){
+      this.pairsClicked += 1;
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      this.pairsClicked += 1;
+      return false;
+    }
   }
-
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed * 2 === this.cards.length) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
